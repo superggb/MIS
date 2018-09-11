b4691f9cd066167f78e5a486f28bcca0bda650d2
thingsareright <maxthingsareright@gmail.com>
2018-09-11 16:23:45 +0800

    应张老板的请求，做了一些修改。

--- src/test/java/service/impl/MoneyRelatedServiceImplTest.java
+++ src/test/java/service/impl/MoneyRelatedServiceImplTest.java
@@ -1,11 +1,14 @@
 package service.impl;
 
+import dto.SalaryShow;
 import entity.Salary;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration({"classpath:spring/spring-*.xml"})
@@ -16,12 +19,12 @@ public class MoneyRelatedServiceImplTest {
 
     @Test
     public void test(){
-        /*List<SalaryShow> salaryShows = moneyRelatedService.findSalaryOrderByDate(0,2);
+        List<SalaryShow> salaryShows = moneyRelatedService.findSalaryOrderByDate(0,2);
         for (SalaryShow salaryShow:
               salaryShows) {
             System.out.println(salaryShow);
-        }*/
+        }
        // moneyRelatedService.updateSalary(100013,11100,12000,"升职加薪！");
-        moneyRelatedService.addSalaryRecord(new Salary(100007,12000.9f,"入职了"));
+       // moneyRelatedService.addSalaryRecord(new Salary(100007,12000.9f,"入职了"));
     }
 }
